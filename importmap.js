dd44912dd5579c879c6dd56157e989cf214e7801
Shannon Poole <shannon.m.poole@gmail.com>
2023-09-22 15:28:43 -0400

    updated revelry engine version

--- importmap.js
+++ importmap.js
@@ -1,19 +1,15 @@
-const shim = document.createElement('script');
-shim.async = true;
-shim.src   = 'https://ga.jspm.io/npm:es-module-shims@1.6.2/dist/es-module-shims.js';
-
 const element = document.createElement('script');
-element.type = 'importmap-shim';
+element.type = 'importmap';
 element.textContent = JSON.stringify({
     imports: {
-        'revelryengine/ecs/'                 : 'https://cdn.jsdelivr.net/gh/revelryengine/ecs@v0.1.0-alpha/',
-        'revelryengine/core/'                : 'https://cdn.jsdelivr.net/gh/revelryengine/core@v0.1.0-alpha/',
-        'revelryengine/gltf/'                : 'https://cdn.jsdelivr.net/gh/revelryengine/gltf@v0.1.0-alpha/',
-        'revelryengine/renderer/'            : 'https://cdn.jsdelivr.net/gh/revelryengine/renderer@v0.1.3-alpha/',
-        'lit'                                : 'https://cdn.skypack.dev/lit@2.0.2',
-        'lit/'                               : 'https://cdn.skypack.dev/lit@2.0.2/',
+        'revelryengine/ecs/'      : 'https://cdn.jsdelivr.net/gh/revelryengine/ecs@v0.2.0-alpha/',
+        'revelryengine/core/'     : 'https://cdn.jsdelivr.net/gh/revelryengine/core@v0.2.0-alpha/',
+        'revelryengine/gltf/'     : 'https://cdn.jsdelivr.net/gh/revelryengine/gltf@v0.1.3-alpha/',
+        'revelryengine/renderer/' : 'https://cdn.jsdelivr.net/gh/revelryengine/renderer@v0.2.0-alpha/',
+        "gl-matrix"               : "https://cdn.jsdelivr.net/gh/toji/gl-matrix@v3.4.1/src/index.js",
+        'lit'                     : 'https://cdn.skypack.dev/lit@2.0.2',
+        'lit/'                    : 'https://cdn.skypack.dev/lit@2.0.2/',
+        
     }
 });
-
-document.currentScript.after(shim);
 document.currentScript.after(element);
